0d5c40e5656e700141305f3c35b3f59117162583
Dang Nguyen <dang.nguyen@nfq.asia>
2021-05-26 13:55:00 +0700

    Update docker-compose: able to run development with docker

--- config/database.js
+++ config/database.js
@@ -17,15 +17,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', 'mongo'),
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'onsread-blog'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        database: env('DATABASE_NAME', 'onpsread-blog'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
         ssl: env.bool('DATABASE_SSL', false),
       },
     },
--- plugins/wysiwyg/admin/src/components/CKEditor.js
+++ plugins/wysiwyg/admin/src/components/CKEditor.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import styled from 'styled-components';
