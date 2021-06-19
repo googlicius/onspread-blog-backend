799f110d9de8a89b4ddc8a091e21ad826c60adc7
Dang Nguyen <dang.nguyen@nfq.asia>
2021-06-19 17:22:32 +0700

    Fix configuration

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'bf8beec8d4a0c36d067da8a314dfed91'),
